f42bcf4dfb66b18db277b8015993a11cad0bdd2e
Måns Rasmussen <42514622+Monras@users.noreply.github.com>
2018-08-19 21:05:07 +0200

    Update index.js

--- src/routes/home/content/projects/2_modelrockets/index.js
+++ src/routes/home/content/projects/2_modelrockets/index.js
@@ -1,7 +1,7 @@
 import urlModelRocketsImage from "./modelrocket.jpg";
 
 export default {
-  title: "Surtr - model rockets",
+  title: "Surtr - model rocketry",
   content: `
   During the fall of 2017, we started up a model rocketry project. The goal is to give our less experienced members, who maybe never been able or gotten a chance to build a model rocket, a leap into what rocket building is all about! It is also a perfect start to your rocket building carrer later proceeding to one of our larger projects, such as Odin. 
 
